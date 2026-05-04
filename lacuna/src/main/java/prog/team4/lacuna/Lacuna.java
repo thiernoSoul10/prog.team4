03ce2c2c33e7b4b8ee70020c7fd326d8ecfd5f72
thsba <thsba10@gmail.com>
2026-05-04 15:57:16 +0200

    First Commit !

---
+++ lacuna/src/main/java/prog/team4/lacuna/Lacuna.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prog.team4.lacuna;
+
+/**
+ *
+ * @author thsba
+ */
+public class Lacuna {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
